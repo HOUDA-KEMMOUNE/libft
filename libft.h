972c0858e90aefd809393f58160b86336a8be589
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2024-10-30 05:05:03 +0000

    Adding ft_memmove prototype

--- libft.h
+++ libft.h
@@ -38,5 +38,6 @@ void    *ft_memset(void *b, int c, size_t len);
 void    ft_bzero(void *s, size_t n);
 void    *ft_calloc(size_t count, size_t size);
 void    *ft_memcpy(void *dst, const void *src, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
