7c62dae89e0c35b103731a2ac40081d1680a687f
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2024-10-31 16:13:17 +0000

    ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 15:10:31 by hkemmoun          #+#    #+#             */
+/*   Updated: 2024/10/31 15:10:35 by hkemmoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    unsigned char *data;
+    char character; 
+    size_t i;
+
+    data = (unsigned char *)s;
+    character = (char)c;
+    i = 0;
+
+    while (i < n)
+    {
+        if (character == data[i])
+        {
+            return (&data[i]);
+        }
+        i++;
+    }
+    if (character == '\0')
+    {
+        return (&data[i]);
+    }
+    else
+    {
+        return (NULL);
+    }
+    
+}
+/*
+int main()
+{
+    char s[] = "salamo3alikom";
+    int c = 'i';
+    size_t n = 5;
+
+    char *r = ft_memchr(s, c, n);
+    if (r) {
+        printf("Return: %s\n", r);
+    } else {
+        printf("Character '%c' not found in the first %zu bytes.\n", c, n);
+    }
+}
+*/
