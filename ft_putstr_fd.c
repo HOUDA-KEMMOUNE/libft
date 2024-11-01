289b103c54d1aebc6710f070f89b0308d3e3b93c
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2024-11-01 15:05:44 +0000

    ft_putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -14,5 +14,21 @@
 
 void ft_putstr_fd(char *s, int fd)
 {
+    size_t i;
     
+    i = 0;
+    while (s[i])
+    {
+        write(fd, &s[i], 1);
+        i++;
+    }
 }
+/*
+int main()
+{
+    char *s = "gdshjdsfgsdfg";
+    int fd = 1;
+    ft_putstr_fd(s, fd);
+    printf("\n");
+}
+*/
