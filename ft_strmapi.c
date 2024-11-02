47efc65a833d2d4c43699eb9ddeb9f1bf9763754
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2024-11-02 10:12:02 +0000

    ft_strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -14,17 +14,43 @@
 
 char f(unsigned int i, char c)
 {
-    if (i % 2 == 0)
+    if (i % 2 == 0 && (c >= 97 && c <= 122))
     {
         return (c - 32);
     }
-    else
+    else if (i % 2 == 1 && (c >= 65 && c <= 90))
     {
         return (c + 32);
     }
+    return (c);
 }
 
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+    char *ptr_s;
+    unsigned int i;
+    char *ptr;
+    size_t len;
 
+    ptr_s = (char *)s;
+    i = 0;
+    len = ft_strlen(ptr_s);
+    ptr = malloc((len + 1) * sizeof(char));
+
+    while (ptr_s[i])
+    {
+        ptr[i] = f(i, ptr_s[i]);
+        i++;
+    }
+    return (ptr);
 }
+/*
+int main()
+{
+    const char *s = "769hb";
+    char *r = ft_strmapi(s, f);
+
+    printf("%s\n", r);
+    free(r);
+}
+*/
