8630312469a08d477aa2c532a6ad19ecb6030e77
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2024-11-02 13:16:45 +0000

    Adding the ft_strtrim prototype

--- libft.h
+++ libft.h
@@ -53,6 +53,7 @@ int     ft_memcmp(const void *s1, const void *s2, size_t n);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char    *ft_strtrim(char const *s1, char const *set);
 void    ft_striteri(char *s, void (*f)(unsigned int, char*));
 void    ft_putchar_fd(char c, int fd);
 void    ft_putstr_fd(char *s, int fd);
