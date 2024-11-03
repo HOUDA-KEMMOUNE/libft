44e2063f38baab1594b56d74f75b29698b458b71
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 10:20:13 +0100

    update

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,19 +6,19 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 18:10:42 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/10/22 18:10:45 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 10:19:27 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isascii(int c)
+int	ft_isascii(int c)
 {
-    if (c >= 0 && c <= 127)
-    {
-        return (1);
-    }
-    return (0);
+	if (c >= 0 && c <= 127)
+	{
+		return (1);
+	}
+	return (0);
 }
 /*
 int main()
@@ -29,4 +29,3 @@ int main()
     printf("%d\n", r);
 }
 */
-
