3ee30c059f24d1b66d9f965feccd579fa363603f
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 11:30:35 +0100

    update

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,23 +6,20 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 18:48:58 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/10/23 12:47:30 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 11:30:06 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t  ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    while (s[i])
-    {
-        i++;
-    }
-
-    return (i);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 /*
 int main()
