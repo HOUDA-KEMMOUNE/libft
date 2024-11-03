78479aa1f40d3b89a6ace4763c5f4a56f2a8c653
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 10:15:55 +0100

    update

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,21 +6,21 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 17:49:38 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/10/22 17:49:41 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 10:15:27 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isalnum(int c)
+int	ft_isalnum(int c)
 {
-    if ((c >= 0 && c <= 9) ||
-        (c >= 65 && c <= 90) ||
-        (c >= 97 && c <= 122))
-    {
-        return (1);
-    }
-    return (0);
+	if ((c >= 0 && c <= 9)
+		|| (c >= 65 && c <= 90)
+		|| (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
 }
 /*
 int main()
