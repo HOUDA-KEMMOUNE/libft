4861c86ff16180f8d9e37b1f68213c5ed394ebc0
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 11:48:34 +0100

    update

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,48 +6,37 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 19:22:29 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/10/24 19:22:33 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 11:48:07 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-    size_t len; //for the length of s
-    size_t e; //for the last index in c
-    size_t b; //from the character to the end of the string
-    size_t b2;
+	size_t	len;
+	size_t	e;
+	size_t	b;
+	size_t	b2;
 
-    len = 0;
-    while (s[len])
-    {
-        len++;
-    }
-
-    b = 0;
-    e = len;
-    while (s[e] != s[0])
-    {
-        if (s[e] == (char)c)
-        {
-            b = e;
-            b2 = 0;
-            while (s[b + b2])
-            {
-                b2++;
-            }
-            return ((char *)&s[b]);
-        }
-        e--;
-    }
-
-    if ((char)c == s[0])
-    {
-        return ((char *)&s[e]);
-    }
-
-    return (NULL);
+	len = ft_strlen(s);
+	b = 0;
+	e = len;
+	while (s[e] != s[0])
+	{
+		if (s[e] == (char)c)
+		{
+			b = e;
+			b2 = 0;
+			while (s[b + b2])
+				b2++;
+			return ((char *)&s[b]);
+		}
+		e--;
+	}
+	if ((char)c == s[0])
+		return ((char *)&s[e]);
+	return (NULL);
 }
 /*
 int main()
