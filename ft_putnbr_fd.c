141c5dc7db9afb2ab8b4eaca1193470c720a8040
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 10:48:29 +0100

    update

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,34 +6,29 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 08:51:57 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/02 08:52:02 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 10:47:58 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-    if (n >= 10)
-    {
-        ft_putnbr_fd(n / 10, fd);
-        ft_putchar_fd((n % 10) + '0', fd);
-
-    }
-    else if (n == INT_MIN)
-    {
-        write(fd, "-2147483648", 11);
-    }
-    else if (n < 0)
-    {
-        write(fd, "-", 1);
-        n = n * -1;
-        ft_putnbr_fd(n, fd);
-    }
-    else
-    {
-        ft_putchar_fd(n + '0', fd);
-    }
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd((n % 10) + '0', fd);
+	}
+	else if (n == INT_MIN)
+		write(fd, "-2147483648", 11);
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = n * -1;
+		ft_putnbr_fd(n, fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
 }
 /*
 int main()
