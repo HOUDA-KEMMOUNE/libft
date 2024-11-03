d32e644cd34131744a30d3d9926a5c369b77ac8d
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 10:32:36 +0100

    update

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,31 +6,28 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 17:17:51 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/10/31 17:17:53 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 10:32:08 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    unsigned char *ptr_s1;
-    unsigned char *ptr_s2;
-    size_t i;
+	unsigned char	*ptr_s1;
+	unsigned char	*ptr_s2;
+	size_t			i;
 
-    ptr_s1 = (unsigned char *)s1;
-    ptr_s2 = (unsigned char *)s2;
-    i = 0;
-
-    while (i < n)
-    {
-        if (ptr_s1[i] != ptr_s2[i])
-        {
-            return (ptr_s1[i] - ptr_s2[i]);
-        }
-        i++;
-    }
-    return (0);
+	ptr_s1 = (unsigned char *)s1;
+	ptr_s2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr_s1[i] != ptr_s2[i])
+			return (ptr_s1[i] - ptr_s2[i]);
+		i++;
+	}
+	return (0);
 }
 /*
 int main()
