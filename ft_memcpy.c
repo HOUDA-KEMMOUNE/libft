8e47a3be7dc2fee279aa37579b44c5ccfe4fca2e
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 10:35:10 +0100

    update

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,31 +6,27 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 09:20:50 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/10/29 09:20:54 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 10:34:45 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-    unsigned char *ptr_src;
-    unsigned char *ptr_dst;
-    size_t i;
-    //size_t j;
+	unsigned char	*ptr_src;
+	unsigned char	*ptr_dst;
+	size_t			i;
 
-    ptr_src = (unsigned char *)src;
-    ptr_dst = (unsigned char *)dst;
-
-    i = 0;
-    while (i < n)
-    {
-        //*(ptr_dst + i) = *(ptr_src + i);
-        ptr_dst[i] = ptr_src[i];
-        i++;
-    }
-
-    return (ptr_dst);
+	ptr_src = (unsigned char *)src;
+	ptr_dst = (unsigned char *)dst;
+	i = 0;
+	while (i < n)
+	{
+		ptr_dst[i] = ptr_src[i];
+		i++;
+	}
+	return (ptr_dst);
 }
 /*
 int main()
