c59e357fda2f9f464a2ba7b53b67ceaae13be330
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 10:59:22 +0100

    update

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,34 +6,30 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 11:13:14 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/02 11:13:18 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 10:58:58 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static  void    f(unsigned int i, char *c)
+static void	f(unsigned int i, char *c)
 {
-    while (c[i])
-    {
-        if (i % 2 == 0 && (c[i] >= 97 && c[i] <= 122))
-        {
-            c[i] = c[i] - 32;
-        }
-        else if (i % 2 == 1 && (c[i] >= 65 && c[i] <= 90))
-        {
-            c[i] = c[i] + 32;
-        }
-        i++;
-    }
+	while (c[i])
+	{
+		if (i % 2 == 0 && (c[i] >= 97 && c[i] <= 122))
+			c[i] = c[i] - 32;
+		else if (i % 2 == 1 && (c[i] >= 65 && c[i] <= 90))
+			c[i] = c[i] + 32;
+		i++;
+	}
 }
 
-void ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-    unsigned int i;
-    
-    i = 0;
-    f(i, s);
+	unsigned int	i;
+
+	i = 0;
+	f(i, s);
 }
 /*
 int main()
