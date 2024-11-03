3f43e1eed36a17a615caaa541437a55c510cbee1
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 11:40:18 +0100

    update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,39 +6,33 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 07:57:50 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/10/25 07:57:52 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 11:39:53 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    size_t i;
-    size_t j;
+	size_t	i;
+	size_t	j;
 
-    i = 0;
-    j = 0;
-
-    if (needle[j] == '\0')
-    {
-        return ((char *)&haystack[i]);
-    }
-
-    while (haystack[i] && i < len - 1)
-    {
-        while (needle[j] == haystack[i + j])
-        {
-            j++;
-            if (needle[j] == '\0')
-            {
-                return ((char *)&haystack[i]);
-            }
-        }
-        j = 0;
-        i++;
-    }
-    return (NULL);
+	i = 0;
+	j = 0;
+	if (needle[j] == '\0')
+		return ((char *)&haystack[i]);
+	while (haystack[i] && i < len - 1)
+	{
+		while (needle[j] == haystack[i + j])
+		{
+			j++;
+			if (needle[j] == '\0')
+				return ((char *)&haystack[i]);
+		}
+		j = 0;
+		i++;
+	}
+	return (NULL);
 }
 /*
 int main()
