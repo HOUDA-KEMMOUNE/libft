b190ed90ec1bf677c9f84d756101a41caf7cb116
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 11:28:56 +0100

    update

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,33 +6,26 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 19:09:34 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/10/23 12:45:58 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 11:28:26 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-    size_t i; //for the length of src
-    size_t j;
+	size_t	src_len;
+	size_t	j;
 
-    j = 0;
-    while (src[j] && j < dstsize - 1)
-    {
-        dst[j] = src[j];
-        j++;
-    }   
-    dst[j] = '\0';
-
-    i = 0;
-    while (src[i])
-    {
-        i++;
-    }
-    
-    //printf("%s\n", dst);
-    return (i); 
+	j = 0;
+	while (src[j] && j < dstsize - 1)
+	{
+		dst[j] = src[j];
+		j++;
+	}
+	dst[j] = '\0';
+	src_len = ft_strlen(src);
+	return (src_len);
 }
 /*
 int main() 
