9951fd286ca332f4ead79542d8adc98cd985edd3
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 10:39:20 +0100

    update

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,44 +6,37 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 06:01:27 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/10/30 06:01:31 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 10:38:16 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-    char *dst_ptr;
-    char *src_ptr;
-    size_t i;
-    
-    if (!src && !dst)
-    {
-        return (NULL);
-    }
+	char	*dst_ptr;
+	char	*src_ptr;
+	size_t	i;
 
-    dst_ptr = (char *)dst;
-    src_ptr = (char *)src;
-    i = 0;
-
-    if (dst_ptr > src_ptr)
-    {
-        while (len-- > 0)
-        {
-            dst_ptr[len] = src_ptr[len];
-        }
-    }
-    else
-    {
-        //ft_memcpy(dst, src, len);
-        while (i < len)
-        {
-            dst_ptr[i] = src_ptr[i];
-            i++; 
-        }
-    }
-    return (dst);
+	if (!src && !dst)
+		return (NULL);
+	dst_ptr = (char *)dst;
+	src_ptr = (char *)src;
+	i = 0;
+	if (dst_ptr > src_ptr)
+	{
+		while (len-- > 0)
+			dst_ptr[len] = src_ptr[len];
+	}
+	else
+	{
+		while (i < len)
+		{
+			dst_ptr[i] = src_ptr[i];
+			i++;
+		}
+	}
+	return (dst);
 }
 /*
 int main()
