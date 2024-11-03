926f17c186dab7c40a364e18564bb4e340107573
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-03 12:05:42 +0100

    update

--- ft_substr.c
+++ ft_substr.c
@@ -6,41 +6,34 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 12:27:35 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/01 12:27:41 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/03 12:05:26 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    size_t s_len;
-    size_t i;
-    char *str;
-    char *ptr;
+	size_t	s_len;
+	size_t	i;
+	char	*str;
+	char	*ptr;
 
-    s_len = ft_strlen(s);
-    str = (char *)s;
-    ptr = malloc(len + 1);
-
-    if (!ptr)
-    {
-        return (NULL);
-    }
-    if (start > s_len)
-    {
-        return (ft_strdup(""));
-    }
-
-    i = 0;
-    while (i < len)
-    {
-        ptr[i] = str[i + start];
-        i++;
-    }
-
-    ptr[i] = '\0';
-    return (ptr);
+	s_len = ft_strlen(s);
+	str = (char *)s;
+	ptr = malloc(len + 1);
+	if (!ptr)
+		return (NULL);
+	if (start > s_len)
+		return (ft_strdup(""));
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = str[i + start];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
 /*
 int main()
