3c9828a5dd75ff11a9288a3ed59df2cb140384e9
Houda Kemmoune <hkemmoun@e2r10p9.1337.ma>
2024-11-07 13:24:49 +0100

    update

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 11:13:14 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/03 10:58:58 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/07 13:24:33 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,10 @@
 
 // static void	f(unsigned int i, char *c)
 // {
-// 	while (c[i])
-// 	{
 // 		if (i % 2 == 0 && (c[i] >= 97 && c[i] <= 122))
 // 			c[i] = c[i] - 32;
 // 		else if (i % 2 == 1 && (c[i] >= 65 && c[i] <= 90))
 // 			c[i] = c[i] + 32;
-// 		i++;
-// 	}
 // }
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
@@ -31,7 +27,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	i = 0;
 	while (s[i])
 	{
-		f(i, s);
+		f(i, &s[i]);
 		i++;
 	}
 }
