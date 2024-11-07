87a19cb88ec0abfdf157383ce2d14c1ad69913bb
Houda Kemmoune <hkemmoun@e2r10p9.1337.ma>
2024-11-07 12:54:15 +0100

    update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 07:57:50 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/07 11:45:16 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:53:54 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	j = 0;
 	if (needle[j] == '\0')
 		return ((char *)&haystack[i]);
-	while (haystack[i] && i < len - 1)
+	while (haystack[i] && i < len)
 	{
-		while (haystack[i + j] == needle[j])
+		while (haystack[i + j] == needle[j] && (i + j) < len)
 		{
 			j++;
 			if (needle[j] == '\0')
