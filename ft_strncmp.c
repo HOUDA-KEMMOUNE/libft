f1ae74091b4f29b7b9944360d36a92e54d7f3b90
Houda Kemmoune <hkemmoun@e2r10p9.1337.ma>
2024-11-07 12:24:29 +0100

    update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:50:22 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/07 11:18:03 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:24:12 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*ptr_s1;
+	unsigned char	*ptr_s2;
 
+	ptr_s1 = (unsigned char *)s1;
+	ptr_s2 = (unsigned char *)s2;
 	i = 0;
-	while ((s1[i] || s2[i]) && (i < n))
+	while ((ptr_s1[i] || ptr_s2[i]) && (i < n))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (ptr_s1[i] != ptr_s2[i])
+			return (ptr_s1[i] - ptr_s2[i]);
 		i++;
 	}
 	return (0);
