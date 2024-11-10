b18b959f22b56f306ca5344e1519fb6e3e2ab2bb
Houda Kemmoune <hkemmoun@c4r6p2.1337.ma>
2024-11-10 12:37:58 +0100

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 16:31:06 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/10 01:25:38 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/10 12:37:20 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,7 @@ t_list		*ft_lstnew(void *content);
 void		ft_lstadd_front(t_list **lst, t_list *new);
 int			ft_lstsize(t_list *lst);
 t_list		*ft_lstlast(t_list *lst);
-void 		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+
 #endif
