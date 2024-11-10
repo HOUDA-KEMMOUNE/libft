c36b79eb3b3f96058ff89a2676751f9713a316e8
Houda Kemmoune <hkemmoun@c4r9p10.1337.ma>
2024-11-10 02:23:32 +0100

    update

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 01:25:04 by hkemmoun          #+#    #+#             */
+/*   Updated: 2024/11/10 02:21:16 by hkemmoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*tail;
+
+	tail = ft_lstlast(*lst);
+	if (tail == NULL)
+		*lst = new;
+	tail->next = new;
+}
