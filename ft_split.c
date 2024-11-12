7ccadc9173bcb5b234920e2f2de503afe479c84a
Houda Kemmoune <hkemmoun@c4r3p5.1337.ma>
2024-11-12 16:25:18 +0100

    update

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 08:45:19 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/12 08:56:58 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/12 16:24:54 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ int	word_count(const char *s, char c)
 
 	i = 0;
 	count = 0;
+	while (s[i] == c)
+		i++;
 	while (s[i])
 	{
-		if (s[i] == c)
+		if (s[i] != c && s[i - 1] == c)
 			count++;
 		i++;
 	}
@@ -29,19 +31,49 @@ int	word_count(const char *s, char c)
 		count++;
 	return (count);
 }
-/*
-char	**ft_split(const char *s, char c)
-{
 
+void	ft_free(char **res, int j)
+{
+	j = 0;
+	while (res[j])
+	{
+		free (res);
+		j++;
+	}
+	free (res);
 }
-*/
 
-
-int main()
+void	fill(const char *str, char c, char **res)
 {
-	const char *s = "haha, hoho, hihi";
-	char c = ',';
-	
-	int count = word_count(s, c);
-	printf("Count of s:\t%d\n", count);
+	int	start;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (str[i] == c)
+			i++;
+		start = i;
+		while (str[i] != c && str[i])
+			i++;
+		res[j] = malloc(sizeof(char) * (i - start + 1));
+		if (!res[j])
+			ft_free(res, j);
+		ft_strlcpy(res[j], str + start, i - start + 1);
+		j++;
+	}
+	res[j] = NULL;
+}
+
+char	**ft_split(const char *str, char c)
+{
+	char	**res;
+
+	res = malloc(sizeof(char *) * (word_count(str, c) + 1));
+	if (!str || !res)
+		return (NULL);
+	fill(str, c, res);
+	return (res);
 }
