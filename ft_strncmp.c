26cc133a54831f7e94279fb5f914c8a96e9a7c50
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2024-11-16 10:08:31 +0100

    update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,13 +6,10 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:50:22 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/07 12:24:12 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/16 10:08:09 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//standard library header file
-//#include <stdio.h>
-//user-defined header file
 #include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
@@ -21,12 +18,16 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*ptr_s1;
 	unsigned char	*ptr_s2;
 
+	if (n == 0)
+		return (0);
+	if (!s1 && !s2)
+		return(0);
 	ptr_s1 = (unsigned char *)s1;
 	ptr_s2 = (unsigned char *)s2;
 	i = 0;
-	while ((ptr_s1[i] || ptr_s2[i]) && (i < n))
+	while ((s1[i] || s2[i]) && (i < n))
 	{
-		if (ptr_s1[i] != ptr_s2[i])
+		if (s1[i] != s2[i])
 			return (ptr_s1[i] - ptr_s2[i]);
 		i++;
 	}
