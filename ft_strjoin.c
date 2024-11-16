330a67f75574fd1838d78dbce35bcdcfc689787f
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2024-11-16 09:09:42 +0100

    update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 14:44:14 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/15 10:43:39 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/16 09:09:27 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ptr;
+	int	total_len;
 
 	if (!s1 || !s2)
 		return (NULL);
-	ptr = (char *)calloc(ft_strlen(s1) + ft_strlen(s2) + 1, 1);
+	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	ptr = (char *)malloc(total_len * sizeof(char));
 	if (!ptr)
 		return (NULL);
 	ft_strlcpy(ptr, s1, ft_strlen(s1) + 1);
