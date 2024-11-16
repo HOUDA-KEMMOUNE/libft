357da1125c68db2585fe189af2a153862936cb95
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2024-11-16 09:05:15 +0100

    update

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 22:14:57 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/08 17:27:35 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/16 09:04:35 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strdup(const char *s1)
 	char	*ptr;
 
 	len = ft_strlen(s1);
-	ptr = (char *)malloc(len + 1);
+	ptr = (char *)malloc((len + 1) * (sizeof(char)));
 	if (!ptr)
 		return (NULL);
 	i = 0;
