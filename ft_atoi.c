09acc446a07926b4203a33b6bbe53713e6c54d74
Houda Kemmoune <hkemmoun@e2r7p5.1337.ma>
2024-11-16 19:44:16 +0100

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,26 +6,32 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 10:54:17 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/13 08:17:53 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/16 19:44:01 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+static int	check(const char *str, int i)
 {
-	int	i;
-	int	sign;
-	int	result;
-
-	i = 0;
-	sign = 1;
-	result = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	if (str[i] == '+')
 		i++;
-	else if (str[i] == '-')
+	return (i);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		sign;
+	long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	i = check(str, i);
+	if (str[i] == '-')
 	{
 		sign = -1;
 		i++;
@@ -33,7 +39,11 @@ int	ft_atoi(const char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = result * 10 + (str[i] - '0');
+		if (sign == 1 && result > INT_MAX)
+			return (-1);
+		else if (sign == -1 && result < INT_MIN)
+			return (0);
 		i++;
 	}
-	return (result * sign);
+	return ((int)result * sign);
 }
