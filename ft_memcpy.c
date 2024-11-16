aa36aa3fab3a175f245f78f123e97a0135367625
Houda Kemmoune <hkemmoun@e2r7p5.1337.ma>
2024-11-16 18:48:08 +0100

    update

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 09:20:50 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/06 18:53:21 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/16 18:47:45 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*ptr_dst;
 	size_t			i;
 
+	if (!dst && !src)
+		return (NULL);
 	ptr_src = (unsigned char *)src;
 	ptr_dst = (unsigned char *)dst;
 	i = 0;
