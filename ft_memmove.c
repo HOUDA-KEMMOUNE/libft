af4680c8c9f3007a0f568be27c1096a321f1efdf
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2024-11-16 08:54:08 +0100

    update

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 06:01:27 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/15 10:26:40 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/16 08:53:48 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	unsigned char	*dst_ptr;
 	unsigned char	*src_ptr;
 
-	if (!src || !dst)
+	if (!src && !dst)
 		return (NULL);
 	dst_ptr = (unsigned char *)dst;
 	src_ptr = (unsigned char *)src;
