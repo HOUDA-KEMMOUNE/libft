6a29300e35173b8b1857f714b980599be409965d
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2024-11-16 01:02:02 +0100

    update

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 01:25:04 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/15 10:30:29 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/16 01:01:50 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	if (!new)
 		return ;
 	tail = ft_lstlast(*lst);
-	if (!tail)
-		return ;
 	if (tail == NULL)
 		*lst = new;
 	tail->next = new;
