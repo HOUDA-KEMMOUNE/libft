491e2ee807eab61f4adc66e2f9f49a5f2f310104
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2024-11-18 16:20:00 +0100

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 10:54:17 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/16 19:47:54 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/18 09:33:50 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_atoi(const char *str)
 {
 	int		i;
 	int		sign;
-	long	result;
+	int		result;
 
 	i = 0;
 	sign = 1;
@@ -39,10 +39,6 @@ int	ft_atoi(const char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = result * 10 + (str[i] - '0');
-		if (sign == 1 && result > INT_MAX)
-			return (-1);
-		else if (sign == -1 && result < INT_MIN)
-			return (0);
 		i++;
 	}
 	return ((int)result * sign);
