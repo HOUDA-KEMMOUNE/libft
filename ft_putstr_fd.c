1c00f0cfbfb7e7bcb5494a7deccf49f2f6383a58
Houda Kemmoune <hkemmoun@e2r10p4.1337.ma>
2024-11-18 23:28:26 +0100

    update

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 15:57:45 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/16 19:59:07 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/18 23:27:58 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	size_t	i;
+
+	if (!s)
+		return ();
 	if (fd >= 0)
 	{
-		size_t	i;
-
 		i = 0;
 		while (s[i])
 		{
